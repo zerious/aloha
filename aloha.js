bfea7cbccac7f2b12c9b11f207f2feb468410c54
Sam Eubank <sameubank+zerious@gmail.com>
2014-10-08 19:27:56 -0700

    Lighter.io module standardization

--- aloha.js
+++ aloha.js
@@ -1,7 +1,32 @@
-var api = module.exports;
+/**
+ * Aloha takes a string in the base language and translates it to the target language.
+ */
+var aloha = module.exports = function (text, lang) {
+  return text + ' in ' + lang;
+};
 
 /**
- * Expose the version to module users.
- * @type {string}
+ * Expose Aloha globally.
  */
-api.version = require('./package.json').version;
+aloha.expose = function (key) {
+  global[key || 'aloha'] = aloha;
+};
+
+/**
+ * Translations are stored in a directory.
+ */
+aloha.dir = process.cwd() + '/translations';
+
+/**
+ * The base language is United States English.
+ */
+aloha.base = 'en-US';
+
+/**
+ * Expose the Aloha version via package.json lazy loading.
+ */
+Object.defineProperty(aloha, 'version', {
+  get: function () {
+    return require('./package.json').version;
+  }
+});
